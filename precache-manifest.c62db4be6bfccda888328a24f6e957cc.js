5ce32e3b498b9bfe0f845b17f313ef22551a225a
Rampraveen Indrajith <ram.praveen16@gmail.com>
2020-05-14 02:06:05 -0400

    Updates

---
+++ precache-manifest.c62db4be6bfccda888328a24f6e957cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139c060199d42068202ff771f05dc47b",
+    "url": "/ClassCover/index.html"
+  },
+  {
+    "revision": "2d5e755dfde2130b3567",
+    "url": "/ClassCover/static/css/main.cc80a921.chunk.css"
+  },
+  {
+    "revision": "c0b552a2895e42c1e799",
+    "url": "/ClassCover/static/js/2.369b3c6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClassCover/static/js/2.369b3c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5e755dfde2130b3567",
+    "url": "/ClassCover/static/js/main.20a99747.chunk.js"
+  },
+  {
+    "revision": "14db1a32c673171f441e",
+    "url": "/ClassCover/static/js/runtime-main.f1d5f1cf.js"
+  }
+]);
